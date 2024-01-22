eed3518de96851387c46bdda5129e8090e324254
Leandro Souza <leandro.souza.159@gmail.com>
2024-01-22 09:04:49 -0300

    Extraindo e salvando dados

--- dados_repos.py
+++ dados_repos.py
@@ -72,3 +72,19 @@ class DadosRepositorios:
         dados['language'] = linguagens
 
         return dados
+
+# Extraindo dados dos repositórios
+
+amazon_rep = DadosRepositorios('amzn')
+ling_mais_usadas_amzn = amazon_rep.cria_df_linguagens()
+
+netflix_rep = DadosRepositorios('netflix')
+ling_mais_usadas_netflix = netflix_rep.cria_df_linguagens()
+
+spotify_rep = DadosRepositorios('spotify')
+ling_mais_usadas_spotify = spotify_rep.cria_df_linguagens()
+
+# Salvando os dados 
+ling_mais_usadas_amzn.to_csv('dados/linguagens_amzn.csv')
+ling_mais_usadas_netflix.to_csv('dados/linguagens_netflix.csv')
+ling_mais_usadas_spotify.to_csv('dados/linguagens_spotify.csv')
